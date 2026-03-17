218ac9603ec71fd001339033014973f76fe08586
Vishnu <myworkspacexx@gmail.com>
2026-03-17 15:23:54 +0530

    api issue solved

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -98,7 +98,7 @@ const EditJobPage = () => {
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <form onSubmit={handleEdit}>
-            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">Update Job Details</h2>
             <div className="mb-4">
               <label htmlFor="type" className="block text-gray-700 font-bold mb-2">Job Type</label>
               <select
